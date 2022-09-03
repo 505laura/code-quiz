dc7458e8f3b9bf16210c13eeedfb15bf432b0f28
505laura <106522200+505laura@users.noreply.github.com>
2022-09-04 08:07:00 +1000

    Add function to switch between pages

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,35 @@ const highscoresPage = document.querySelector('#highscores-page');
 // Array to group the pages together
 const pages = [startPage, questionPages, endPage, highscoresPage];
 
+// Beginning at -1 so that the first page increments to 0
+let page = -1;
+
+// Function to change pages, beginning at the start page
+function pageChange() {
+    // Iterating through the pages array to hide all of the content
+    for (let i = 0; i < pages.length; i++) {
+        pages[i].style.display = 'none';
+    }
+    page++;
+    switch (page) {
+      case 0:
+        startPage.style.display = '';
+        break;
+      case 1:
+        questionPages.style.display = '';
+        break;
+      case 2:
+        endPage.style.display = '';
+        break;
+      case 3:
+        highscoresPage.style.display = '';
+        break;
+      default:
+        startPage.style.display = '';
+        break;
+    }
+}
+
+window.onload = pageChange;
+
+startButton.addEventListener('click', pageChange);
