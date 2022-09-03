5386b1c4f6e7e0e713595bb6b710b4d96b558169
505laura <106522200+505laura@users.noreply.github.com>
2022-09-04 08:05:58 +1000

    Add constants and array to JS file

---
+++ assets/js/script.js
@@ -0,0 +1,12 @@
+// Constant to store the button element on the start page
+const startButton = document.querySelector('#start-button');
+
+// Constants to store the elements from the HTML
+const startPage = document.querySelector('#start-page');
+const questionPages = document.querySelector('#question-pages');
+const endPage = document.querySelector('#end-page');
+const highscoresPage = document.querySelector('#highscores-page');
+
+// Array to group the pages together
+const pages = [startPage, questionPages, endPage, highscoresPage];
+
